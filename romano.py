4b3f3f9eac8b8b51ffa96c241407f4a3f44761c4
GUNNAR FORNAZIER GOMES <00001099296365sp@aluno.educacao.sp.gov.br>
2024-06-26 07:42:50 -0300

    make roman things

--- romano.py
+++ romano.py
@@ -1,8 +1,2 @@
-import rom
-import rom.rom
 
-
-def MakeRomanInt(a):
-    #a = input("Escreva um numero romano: ")
-    a = rom.rom_parse(a)
-    print(a)
+    
--- run.py
+++ run.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, get_template_attribute
+import roman
+
+
+
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     
     return render_template('main.html')
 
 @app.route('/resultado', methods=['GET','POST'])
 def resultado():
-    return render_template('resultado.html', result='MeuPau')
+    response = request.form['numeroRomano']
+    response_str = str(response)
+    response_upper = response_str.upper()
+    transformResponse = roman.fromRoman(response_upper)
+    
+    return render_template('resultado.html', result=transformResponse)
 
 if __name__ == '__main__':
     app.run(debug=True)
